5084a3be2039687c97bf379b4e672d5de6f2eef4
Dan Dudley <born2snipe@gmail.com>
2018-10-04 20:29:46 -0500

    downloaded the examples core and desktop modules so it will be easier to see if I break something

---
+++ examples/flixel-examples-core/src/org/flixel/examples/performance/Performance.java
@@ -0,0 +1,13 @@
+package org.flixel.examples.performance;
+
+import org.flixel.FlxGame;
+
+public class Performance extends FlxGame
+{
+
+	public Performance()
+	{
+		super(480, 800, PlayState.class);
+	}
+	
+}
---
+++ examples/flixel-examples-desktop/src/org/flixel/VibrationDemo.java
@@ -0,0 +1,9 @@
+package org.flixel;
+
+public class VibrationDemo
+{
+	public static void main(String[] args)
+	{
+		new FlxDesktopApplication(new org.flixel.examples.vibration.VibrationDemo(), 480, 320);
+	}
+}
