d7d7303c7770ca4fdcf1d8639b8cd2e67be9b617
Dan Dudley <born2snipe@gmail.com>
2019-06-20 22:38:15 -0500

    updated to properly push tag

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 VERSION=$1
+TAG=v$VERSION
 
 ./mvnw versions:set -DskipTests -DnewVersion=$VERSION -DgenerateBackupPoms=false -T2 \
     && ./mvnw deploy -Prelease -T2 \
     && git add . \
     && git commit -m "Release v. $VERSION" \
-    && git tag -a v$VERSION -m "Release v. $VERSION" \
-    && git push
+    && git tag -a $TAG -m "Release v. $VERSION" \
+    && git push \
+    && git push origin $TAG
     
 
