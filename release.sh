6ee7ebb9458fa3c06d167001abd555633ac298b9
Dan Dudley <born2snipe@gmail.com>
2019-06-21 08:41:00 -0500

    try releasing again...

--- release.sh
+++ release.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
+set -e
+
 VERSION=$1
 TAG=v$VERSION
+PROJECT_LIST=flixel-core,flixel-android,flixel-desktop,flixel-html5,flixel-robovm,plugins/accelerometer,plugins/controllers,plugins/gesture
 
-./mvnw versions:set -DskipTests -DnewVersion=$VERSION -DgenerateBackupPoms=false -T2 \
-    && ./mvnw deploy -Prelease -T2 \
-    && git add . \
-    && git commit -m "Release v. $VERSION" \
-    && git tag -a $TAG -m "Release v. $VERSION" \
-    && git push \
-    && git push origin $TAG
+# update the POM files with the version number
+./mvnw versions:set -DskipTests -DnewVersion=$VERSION -DgenerateBackupPoms=false -T2 -pl $PROJECT_LIST -am
+
+# build jar, sources, javadoc, and push to sonatype for staging
+./mvnw deploy -Prelease -T2 -pl $PROJECT_LIST -am
+
+# commit the changes VC
+git add .
+git commit -m "Release v. $VERSION"
+git tag -a $TAG -m "Release v. $VERSION"
+git push
+git push origin $TAG
     
 
